e6ecd88d0a0a558f4770faf9835c1e715294ad5c
v833 <2507301541@qq.com>
2022-10-30 23:07:36 +0800

    feat: shapeFlags

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,5 @@
 import { isObject } from '../shared/index'
+import { ShapeFlags } from '../shared/shapeFlags'
 import { createComponentInstance, setupComponent } from './component'
 
 export function render(vnode, container) {
@@ -9,10 +10,11 @@ export function render(vnode, container) {
 function patch(vnode, container) {
   // 处理组件
 
-  // 判断是不是element类型
-  if (typeof vnode.type === 'string') {
+  const { shapeFlag } = vnode
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vnode, container)
-  } else if (isObject(vnode.type)) {
+    // 判断是不是element类型
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     processComponent(vnode, container)
   }
 }
@@ -26,27 +28,27 @@ function processElement(vnode, container) {
   mountElement(vnode, container)
 }
 
-function mountComponent(vnode, container) {
-  const instance = createComponentInstance(vnode)
+function mountComponent(initialVNode, container) {
+  const instance = createComponentInstance(initialVNode)
   setupComponent(instance)
-  setupRenderEffect(instance, vnode, container)
+  setupRenderEffect(instance, initialVNode, container)
 }
-function setupRenderEffect(instance, vnode, container) {
+function setupRenderEffect(instance, initialVNode, container) {
   const { proxy, setupState } = instance
   const subTree = instance.render.call(proxy)
   // const subTree = instance.render.call(setupState)
   patch(subTree, container)
-  vnode.el = subTree.el
+  initialVNode.el = subTree.el
   // vnode -> patch
   // vnode -> element -> mountElement
 }
 function mountElement(vnode: any, container: any) {
   const el = (vnode.el = document.createElement(vnode.type))
-  const { props, children } = vnode
+  const { props, children, shapeFlag } = vnode
   // string array
-  if (typeof children === 'string') {
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children
-  } else if (Array.isArray(children)) {
+  } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     mountChildren(children, el)
   }
   for (const key in props) {
