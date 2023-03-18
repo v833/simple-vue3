43271324249e43bde0c84a42d16ac6922e983c90
v833 <2507301541@qq.com>
2023-03-18 18:51:29 +0800

    feat: ref

--- demo/index.js
+++ demo/index.js
@@ -87,7 +87,65 @@ function shallowReadonly(obj) {
   return createReactive(obj, true, true)
 }
 
-const obj = reactive(data)
+function ref(val) {
+  const wrapper = {
+    value: val
+  }
+  Object.defineProperty(wrapper, '__v_isRef', {
+    value: true
+  })
+  return reactive(wrapper)
+}
+
+function toRef(obj, key) {
+  const wrapper = {
+    get value() {
+      return obj[key]
+    },
+    set value(val) {
+      obj[key] = val
+    }
+  }
+  Object.defineProperty(wrapper, '__v_isRef', {
+    value: true
+  })
+  return wrapper
+}
+
+function toRefs(obj) {
+  const res = {}
+  for (let ket in obj) {
+    res[key] = toRef(obj, key)
+  }
+  return res
+}
+
+function isRef(ref) {
+  return !!ref.__v_isRef
+}
+
+function unref(ref) {
+  return isRef(ref) ? ref.value : ref
+}
+
+function proxyRefs(target) {
+  return new Proxy(target, {
+    get(target, key, receiver) {
+      const value = Reflect.get(target, key, receiver)
+      return unref(value)
+    },
+    set(target, key, newValue, receiver) {
+      const value = target[key]
+      if (isRef(value)) {
+        value.value = newValue
+        return true
+      }
+      return Reflect.set(target, key, newValue, receiver)
+    }
+  })
+}
+
+// const obj = reactive(data)
 // TODO 1 happypath
 // effect(() => {
 //   console.log('effect run')
@@ -162,3 +220,10 @@ const obj = reactive(data)
 // arr[0] = 3
 
 // TODO ref
+const refVal = ref(1)
+
+effect(() => {
+  console.log(refVal.value)
+})
+
+refVal.value = 2
